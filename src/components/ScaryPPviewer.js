166aa44fda3fca983782c969cfaf7b4c33a4afa1
armmarrodon <martinrodjuan@gmail.com>
2022-08-25 11:44:25 +0200

    youtube viewerUpdated

--- src/components/ScaryPPviewer.js
+++ src/components/ScaryPPviewer.js
@@ -29,10 +29,12 @@ function ScaryPPviewer(props) {
 
                         </div>
                         <div className="titleProject">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/S9bahVxUr6Y"
+                            <div className="youtubeBox">
+                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/S9bahVxUr6Y"
                                 title="YouTube video player" frameborder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen></iframe>
+                                    allowfullscreen></iframe>
+                                </div>
                         </div>
                     </div>
 
